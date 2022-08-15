600c366c482e54e7baa65e3fc5a9f5021c373827
WilterD <wilterd18@gmail.com>
2022-08-15 15:08:54 -0400

    componente sencillo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")); // app incializada
+
+function Saludar() {
+  function sumar(a, b) {
+    return a + b;
+  }
+
+  return <h1>{sumar(1, 2)}</h1>;
+}
+
+root.render(
+  <div>
+    <Saludar />
+    <Saludar />
+    <Saludar />
+    <Saludar />
+  </div>
+); // renderizar el componente en el DOM
+
+// root.render(<h1>Hola Mundo</h1>); // renderizado de la app
